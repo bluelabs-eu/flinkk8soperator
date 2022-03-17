5b0c45c70035d963070994d9e98c2c1f9d824fd1
sten <nikola@bluelabs.eu>
2022-03-17 18:10:55 +0100

    make adjustments for generate script. Bump the version

--- bluelabs-publish-operator.sh
+++ bluelabs-publish-operator.sh
@@ -1,2 +1,3 @@
-docker buildx build --platform=linux/amd64 -t eu.gcr.io/bluelabs-container-registry/data-lyft-flink-operator:v1.0.6-SNAPSHOT .
-docker push eu.gcr.io/bluelabs-container-registry/data-lyft-flink-operator:v1.0.6-SNAPSHOT
+version=1.0.6
+docker buildx build --platform=linux/amd64 -t "eu.gcr.io/bluelabs-container-registry/data-lyft-flink-operator:v$version" .
+docker push "eu.gcr.io/bluelabs-container-registry/data-lyft-flink-operator:v$version"
--- tmp/codegen/update-generated.sh
+++ tmp/codegen/update-generated.sh
@@ -10,3 +10,8 @@ github.com/lyft/flinkk8soperator/pkg/client \
 github.com/lyft/flinkk8soperator/pkg/apis \
 app:v1beta1 \
 --go-header-file "./tmp/codegen/boilerplate.go.txt"
+
+if [[ "$PWD" != "$GOPATH/src/github.com/lyft/flinkk8soperator" ]]; then
+  echo "Project not located in $GOPATH subdirectory. Copying generated files from $GOPATH/src/github.com/lyft/flinkk8soperator"
+  cp -rf "$GOPATH/src/github.com/lyft/flinkk8soperator/" "$PWD"
+fi
