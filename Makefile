fcdb8e4af2e228340e26993e7b870efa3ba62261
sten <nikola@bluelabs.eu>
2022-04-27 19:04:47 +0200

    Create option to run and build remote debug ready version

--- Makefile
+++ Makefile
@@ -12,8 +12,12 @@ compile: generate
 	go build -o bin/flinkoperator ./cmd/flinkk8soperator/main.go
 
 .PHONY: linux_compile
-linux_compile: generate
+linux_compile:
+ifeq ($(with_debug_flags),true)
+	GOOS=linux GOARCH=amd64 CGO_ENABLED=0 go build -gcflags="all=-N -l" -o /artifacts/flinkoperator ./cmd/flinkk8soperator/main.go
+else
 	GOOS=linux GOARCH=amd64 CGO_ENABLED=0 go build -o /artifacts/flinkoperator ./cmd/flinkk8soperator/main.go
+endif
 
 gen-config:
 	which pflags || (go get github.com/lyft/flytestdlib/cli/pflags)
--- bluelabs-publish-operator.sh
+++ bluelabs-publish-operator.sh
@@ -1,3 +1,20 @@
-version=1.0.6
-docker buildx build --platform=linux/amd64 -t "eu.gcr.io/bluelabs-container-registry/data-lyft-flink-operator:v$version" .
+current_version="1.0.7"
+debug=false
+while true; do
+    read -p "Build as debug version? [no]: " yn
+    case $yn in
+        [Yy]* ) debug=true break;;
+        [Nn]* ) break ;;
+        * ) echo "Using default: Building as regular version" && break;;
+    esac
+done
+
+read -p "Enter version [$current_version]: " version
+
+build_version=${version:-$current_version}
+if [ $debug == true ]; then
+  build_version=${build_version}-debug
+fi
+echo "Building version $build_version, with_debug_flags=$debug"
+docker buildx build --platform=linux/amd64 --build-arg with_debug_flags=$debug -t "eu.gcr.io/bluelabs-container-registry/data-lyft-flink-operator:v$version" .
 docker push "eu.gcr.io/bluelabs-container-registry/data-lyft-flink-operator:v$version"
